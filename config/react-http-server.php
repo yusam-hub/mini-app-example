c2524a1b5d275b3a42648337ce4173f445e67bb7
yusam <yusam@yandex.ru>
2023-03-07 04:50:15 +0400

    #changed

--- config/react-http-server.php
+++ config/react-http-server.php
@@ -2,6 +2,7 @@
 
 return [
     'httpServerConfigModel' => [
+        'isDebugging' => (bool) app_ext_env('APP_IS_DEBUGGING', false),
         'limitConcurrentRequests' => intval(app_ext_env('HTTP_SERVER_LIMIT_CONCURRENT_REQUESTS', 100)),
         'limitRequestBodyBuffer' => intval(app_ext_env('HTTP_SERVER_LIMIT_REQUEST_BODY_BUFFER', 2097152)),
         'socketServerMode' => \YusamHub\AppExt\ReactHttpServer\HttpServerConfigModel::SOCKET_SERVER_MODE_UNIX_FILE,
