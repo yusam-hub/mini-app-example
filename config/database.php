d823fed7c67f098e84c9d592b81f545a78420ca6
yusam <yusam@yandex.ru>
2023-03-10 08:08:46 +0400

    #changed

--- config/database.php
+++ config/database.php
@@ -1,29 +1,23 @@
 <?php
 
 return [
-    'default' => 'mysql',
+    'default' => 'default',
 
     'connections' => [
-        'mysql' => [
+        'default' => [
             'user' => app_ext_env('DATABASE_USER'),
             'password' => app_ext_env('DATABASE_PASSWORD'),
             'host' => app_ext_env('DATABASE_HOST'),
             'port' => app_ext_env('DATABASE_PORT'),
             'dbName' => app_ext_env('DATABASE_DBNAME'),
         ],
-        'mysql2' => [
-            'user' => app_ext_env('DATABASE_USER'),
-            'password' => app_ext_env('DATABASE_PASSWORD'),
-            'host' => app_ext_env('DATABASE_HOST'),
-            'port' => app_ext_env('DATABASE_PORT'),
-            'dbName' => app_ext_env('DATABASE_DBNAME'),
+    ],
+
+    'migrations' => [
+        'paths' => [
+            app()->getRootDir('/vendor/yusam-hub/app-ext/database/migrations'),
+            app()->getDatabaseDir('/migrations'),
         ],
-        'mysql3' => [
-            'user' => app_ext_env('DATABASE_USER'),
-            'password' => app_ext_env('DATABASE_PASSWORD'),
-            'host' => app_ext_env('DATABASE_HOST'),
-            'port' => app_ext_env('DATABASE_PORT'),
-            'dbName' => app_ext_env('DATABASE_DBNAME'),
-        ],
+        'savedDir' => app()->getStorageDir('/app/migrations')
     ],
 ];
