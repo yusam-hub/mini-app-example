eb2c1f5a315184770eb2f83a09635d46670fae70
yusam <yusam@yandex.ru>
2023-03-11 14:39:49 +0400

    #changed

--- config/redis.php
+++ config/redis.php
@@ -1,14 +1,20 @@
 <?php
 
 return [
-    'default' => 'default',
+    'default' => 'app',
 
     'connections' => [
-        'default' => [
+        'app' => [
             'host' => app_ext_env('REDIS_HOST', 'localhost'),
             'port' => app_ext_env('REDIS_PORT', 6379),
             'dbIndex' => 0,
-            'prefix' => 'miniapp_',
+            'prefix' => 'app_',
+        ],
+        'cache' => [
+            'host' => app_ext_env('REDIS_HOST', 'localhost'),
+            'port' => app_ext_env('REDIS_PORT', 6379),
+            'dbIndex' => 1,
+            'prefix' => 'cache_',
         ],
     ],
 ];
