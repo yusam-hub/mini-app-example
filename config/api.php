6edfed38f5b8ef4a9745cb7ebc69c1d883691a38
yusam <yusam@yandex.ru>
2023-03-11 06:10:32 +0400

    #changed

--- config/api.php
+++ config/api.php
@@ -6,45 +6,70 @@ return [
     'apiBaseUri' => '/api',
     'tokenKeyName' => 'X-Token',
     'signKeyName' => 'X-Sign',
-    'tokens' => [
+    'debugTokens' => [
         'testing' => 0 //по токену находим ID
     ],
-    'signs' => [
+    'debugSigns' => [
         0 => 'testing', //по ID находим ключ подписи для ID
     ],
-    'tokenHandle' => function(\Symfony\Component\HttpFoundation\Request $request) {
+    'tokenHandle' => function(
+        \YusamHub\AppExt\Traits\Interfaces\GetSetHttpControllerInterface $httpController,
+        \Symfony\Component\HttpFoundation\Request $request
+    )
+    {
         if ($request->getRequestUri() === '/') {
             return null;
         }
-        $tokens = (array) app_ext_config('api.tokens');
 
-        if (empty($tokens)) {
+        $debugTokens = (array) app_ext_config('api.debugTokens');
+
+        if (empty($debugTokens)) {
             return null;
         }
 
         $tokenValue = (string) $request->headers->get(app_ext_config('api.tokenKeyName'));
 
-        if (!in_array($tokenValue, array_keys($tokens))) {
-            throw new \YusamHub\AppExt\Exceptions\HttpUnauthorizedAppExtRuntimeException([
-                'message' => 'Invalid token value'
-            ]);
+        if (in_array($tokenValue, array_keys($debugTokens))) {
+            return intval($debugTokens[$tokenValue]);
         }
-        return intval($tokens[$tokenValue]);
+
+        $apiUserModel = \YusamHub\AppExt\Db\Model\ApiUserModel::findModelByAttributes($httpController->getDbKernel(), ['apiToken' => $tokenValue]);
+        if (!is_null($apiUserModel)) {
+            return $apiUserModel;
+        }
+
+        throw new \YusamHub\AppExt\Exceptions\HttpUnauthorizedAppExtRuntimeException([
+            'detail' => 'Invalid token value'
+        ]);
     },
-    'signHandle' => function(\Symfony\Component\HttpFoundation\Request $request, int $apiAuthorizedId) {
-        $signs = (array) app_ext_config('api.signs');
+    'signHandle' => function(
+        \YusamHub\AppExt\Traits\Interfaces\GetSetHttpControllerInterface $httpController,
+        \Symfony\Component\HttpFoundation\Request $request,
+        int $apiAuthorizedId,
+        ?\YusamHub\AppExt\Db\Model\ApiUserModel $apiUserModel
+    )
+    {
+        $signValue = (string) $request->headers->get(app_ext_config('api.signKeyName'));
 
-        if (isset($signs[$apiAuthorizedId])) {
+        if (is_null($apiUserModel)) {
 
-            $signValue = (string) $request->headers->get(app_ext_config('api.signKeyName'));
-            //todo: в режиме отладки подписывать не нужно ничего, просто сверяем два ключа как есть, что бы тестировать через сваггер
-            if ($signValue !== $signs[$apiAuthorizedId]) //просто проверяем совпадение, не подписываем параметры, для этого нужен новый пакет для генерации подписи на основе контента или заголовка
+            $debugSigns = (array) app_ext_config('api.debugSigns');
+
+            if (isset($debugSigns[$apiAuthorizedId]) && $signValue !== $debugSigns[$apiAuthorizedId])
             {
                 throw new \YusamHub\AppExt\Exceptions\HttpUnauthorizedAppExtRuntimeException([
-                    'message' => 'Invalid sign value',
-                    'authorizedId' => $apiAuthorizedId
+                    'detail' => 'Invalid debug sign value',
                 ]);
             }
+
+            return;
+        }
+
+        if ($signValue !== $apiUserModel->apiSign)
+        {
+            throw new \YusamHub\AppExt\Exceptions\HttpUnauthorizedAppExtRuntimeException([
+                'detail' => 'Invalid sign value',
+            ]);
         }
     },
     //'infoTitle' => 'Api %s Server',
