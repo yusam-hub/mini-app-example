a9b51a6693bbd7bd66828dfa081156b60a89d59d
yusam <yusam@yandex.ru>
2023-03-19 09:04:41 +0400

    #changed

--- public/static/dev/js/init_end.js
+++ public/static/dev/js/init_end.js
@@ -3,24 +3,5 @@ window.jsWait = new TjsWait(window.jsGlob);
 window.jsPost = new TjsPost(window.jsWait);
 window.jsMsg = new TjsMsg();
 
-/*
-let jsInterval = new TjsInterval(1000, function(){
-    console.log('tick', this.tickCounter);
-    if (this.tickCounter >= 5) {
-        this.stop();
-    }
-});
 
-jsInterval.start();*/
-
-let jsCountdown = new TjsCountdown(
-    5,
-    function (jsCountdown){
-        console.log("finished");
-    },
-    function (jsCountdown){
-        console.log('tick', jsCountdown.formatDateTime());
-    });
-
-jsCountdown.start();
 
