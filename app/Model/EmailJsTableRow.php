ce5904f2bd043243bfa3f0154bb8d42d9a30f0ef
yusam <yusam@yandex.ru>
2023-03-22 08:33:39 +0400

    #changed

--- app/Model/EmailJsTableRow.php
+++ app/Model/EmailJsTableRow.php
@@ -42,69 +42,17 @@ class EmailJsTableRow extends JsTableRow
         $tableResponse->query->sortFieldName = $jsTableQuery->sortFieldName;
         $tableResponse->query->sortDirection = $jsTableQuery->sortDirection;
 
-        $query = <<<MYSQL
-select 
-    *
-from 
-    tmp_emails
-:where_condition
-:order_condition        
-limit :offset, :limit
-MYSQL;
-        $offset = ($tableResponse->query->page - 1) * $tableResponse->query->limit;
-
-        $where_conditions = [];
-        $where_conditions_bindings = [];
-
-        foreach($jsTableQuery->filter as $field => $value) {
-            if (!empty($value)) {
-                $where_conditions[] = sprintf('%s = ?', $field);
-                $where_conditions_bindings[] = $value;
-            }
-        }
-
-        $where_condition = !empty($where_conditions) ? 'where ' . implode(" and ", $where_conditions) : '';
-
-        $order_conditions = [];
-        if (!empty($tableResponse->query->sortFieldName) && !empty($tableResponse->query->sortDirection) && in_array($tableResponse->query->sortDirection, ['asc','desc'])) {
-            $order_conditions[] = sprintf('%s %s', $tableResponse->query->sortFieldName, $tableResponse->query->sortDirection);
-        }
-
-        $order_condition = !empty($order_conditions) ? 'order by ' . implode(", ", $order_conditions) : '';
-
-        $rows = app_ext_db_global()
-            ->pdoExt()
-            ->fetchAll(
-                strtr(
-                    $query,
-                    [
-                        ':where_condition' => $where_condition,
-                        ':order_condition' => $order_condition,
-                        ':offset' => $offset,
-                        ':limit' => $tableResponse->query->limit
-                    ]
-                ),
-                $where_conditions_bindings
-            );
-
         $queryBuilder = app_ext_db_global()->pdoExt()->queryBuilder();
         $queryBuilder
             ->select('*')
             ->from(TABLE_TMP_EMAILS);
-        foreach($jsTableQuery->filter as $field => $value) {
-            if (!empty($value)) {
-                $queryBuilder->andWhere([$field => $value]);
-            }
-        }
-        if (!empty($tableResponse->query->sortFieldName) && !empty($tableResponse->query->sortDirection) && in_array($tableResponse->query->sortDirection, ['asc','desc']))
-        {
-            $queryBuilder->orderBy([
-                $tableResponse->query->sortFieldName => $tableResponse->query->sortDirection
-            ]);
-        }
+        $queryBuilder->where($jsTableQuery->filter);
+        $queryBuilder->orderBy([
+            $tableResponse->query->sortFieldName => $tableResponse->query->sortDirection
+        ]);
         $queryBuilder->offset(($tableResponse->query->page - 1) * $tableResponse->query->limit);
         $queryBuilder->limit($tableResponse->query->limit);
-        //$rows2 = $queryBuilder->fetchAll();
+        $rows = $queryBuilder->fetchAll();
 
         $tableResponse->data->import($rows);
         $tableResponse->detail->count = count($rows);
